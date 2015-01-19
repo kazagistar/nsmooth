bfe3839e9aa56f89b2133f528d21e21c5e0a4045
Jakub Gedeon <kazagistar@gmail.com>
2015-01-18 20:59:13 -0700

    Actually added files :/

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+#![feature(box_syntax)]
+
+extern crate slow_primes;
+extern crate num;
+
+use std::cmp::Ordering;
+use num::integer::Integer;
+use num::traits::One;
+use std::collections::BinaryHeap;
+use std::num::NumCast;
+use std::num::FromPrimitive;
+
+
+#[derive(Copy, Eq, PartialEq)]
+struct Composite <I : Integer + Ord> {
+    product : I,
+    cutoff: usize,
+}
+
+impl <I: Integer + Ord> Ord for Composite<I> {
+    fn cmp(&self, other: &Composite<I>) -> Ordering {
+        other.product.cmp(&self.product)
+    }
+}
+
+impl <I: Integer + Ord> PartialOrd for Composite<I> {
+    fn partial_cmp(&self, other: &Composite<I>) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
+pub struct NSmooth <I : Integer + Ord> {
+    lookahead : BinaryHeap<Composite<I>>,
+    factors : Box<[I]>,
+}
+
+pub fn nsmooth<I : Integer + Ord + FromPrimitive>(n : usize) -> NSmooth<I> {
+    let sieve = slow_primes::Primes::sieve(n);
+    let primes: Vec<I> = sieve.primes().map(|x| <I as FromPrimitive>::from_uint(x).expect("Overflow while generating primes")).collect();
+
+    // for now, we ignore n, until I actually get a prime generator
+    let mut ns = NSmooth {
+        factors: primes.into_boxed_slice(),
+        lookahead: BinaryHeap::new(),
+    };
+    ns.lookahead.push(Composite { product: <I as One>::one(), cutoff: 0 });
+    return ns;
+}
+
+impl <I : Integer + Ord + Clone> Iterator for NSmooth<I> {
+    type Item = I;
+
+    fn next(&mut self) -> Option<I> {
+        let prev = self.lookahead.pop().expect("Error: Number of products was finite?!?");
+        
+        let prime = self.factors[prev.cutoff].clone();
+        // Push first child
+        self.lookahead.push(Composite {
+            product: prev.product.clone() * prime.clone(),
+            cutoff: prev.cutoff,
+        });
+      
+        // Push brother
+        let brother_cutoff = prev.cutoff + 1;
+        if brother_cutoff < self.factors.len() && prev.product != <I as One>::one() {
+            let brother_prime = self.factors[brother_cutoff].clone();
+            self.lookahead.push(Composite {
+                product: prev.product.clone() / prime * brother_prime,
+                cutoff: prev.cutoff + 1,
+            });
+        }
+        return Some(prev.product);
+    }
+}
