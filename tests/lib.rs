2bedf8fe069a62286c98232a27a7dd7af6acdc1d
Jakub Gedeon <jgedeon@sofi.com>
2017-03-20 17:37:57 -0600

    Add test for unique results

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,6 +1,7 @@
 extern crate nsmooth;
-
 extern crate slow_primes;
+
+use std::collections::BTreeSet;
 
 // All smooth numbers are counted as monotonically increasing
 // This means there are no repeats, and no "out of order" problems
@@ -19,11 +20,19 @@ fn strictly_monotonically_increasing() {
 #[test]
 fn valid_factorizations() {
     let primes = slow_primes::Primes::sieve(20);
-    
+
     for now in nsmooth::nsmooth(20).take(1000000) {
         match primes.factor(now) {
             Ok(factors) => { assert!(factors.iter().all(|&(x,_)| x<20)) },
             Err(_)      => { panic!("Bad factor") },
         }
     }
+}
+
+// Make sure generated numbers are unique
+// Tests up to ten thousand
+#[test]
+fn unique_results() {
+    let unique: BTreeSet<i64> = nsmooth::nsmooth(5).take(10000).collect();
+    assert!(unique.len() == 10000);
 }
